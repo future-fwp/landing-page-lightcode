21065a1353426aae60351b90c06819b7099a6b90
future-html <witsanuporn19646@gmail.com>
2025-01-19 09:26:07 +0700

    remove unused react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Experience from "./components/Experience";
+
 import Hero from "./components/Hero";
-import About from "./components/About";
+
 const App = () => {
 	const [showMenuBar, setShowMenuBar] = React.useState<boolean>(false);
 	document.body.style.overflow = showMenuBar ? "hidden" : "auto";
--- src/components/Saving.tsx
+++ src/components/Saving.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Saving = () => {
 	return (
 		<section id="saving-money">
